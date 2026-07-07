3c03d9c1683b857d269bf002518a604f2ce7a78c
Holman Buenaventura <habuenav@hotmail.com>
2026-07-07 02:49:05 -0500

    Improve comments in basicTest.ino
    Updated comments for clarity and readability.

--- examples/basicTest/basicTest.ino
+++ examples/basicTest/basicTest.ino
@@ -1,3 +1,7 @@
+// FILE: basicTest.ino
+// AUTHOR: Holman Buenaventura
+// PURPOSE: basic test of Breath.
+
 #include <Breath.h>
 
 Breath myBreath(5,18);
@@ -12,28 +16,20 @@ void setup()
   Serial.println("Test of Breath");
   minbreath = maxbreath = lastbreath =  myBreath.read();                          
 }
-
 void loop()
 {
-
   int breath =  myBreath.read();                          
-
   if(breath != lastbreath)
   {
     Serial.print("breath="); Serial.print(breath);
-  
     if(breath < minbreath) minbreath = breath;
     if(breath > maxbreath) maxbreath = breath;
-  
     Serial.print("\tmin="); Serial.print(minbreath);
     Serial.print("\tmax="); Serial.print(maxbreath);
-
-    // plot a little graph
+    // plot a little graph to serial monitor
     int reading = (int)((double)(breath - minbreath)/(maxbreath - minbreath + 1) * 50);
     Serial.print("\t\t");
-
     for(int i = 0; i < reading; i++) Serial.print("+");
-
     Serial.println();
     lastbreath = breath;
   }
